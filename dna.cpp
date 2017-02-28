c0f0c61aa0f8aadf559a4cd90c793ae79802aca1
Diego Assencio <diego@assencio.com>
2017-02-28 19:43:17 +0100

    renames variable

--- dna.cpp
+++ dna.cpp
@@ -12,7 +12,7 @@ char* random_dna ()
 	static const char base_array[] = { 'A', 'C', 'G', 'T' };
 
 	std::random_device device;
-	std::mt19937 gen(device());
+	std::mt19937 generator(device());
 
 	/* random DNA length generator */
 	std::uniform_int_distribution<> length_chooser(1,20);
@@ -21,14 +21,14 @@ char* random_dna ()
 	std::uniform_int_distribution<> base_chooser(0,3);
 
 	/* select the DNA sequence length */
-	size_t dna_len = length_chooser(gen);
+	size_t dna_len = length_chooser(generator);
 
 	char* dna_str = new char[dna_len+1];
 
 	/* build the DNA sequence */
 	for (size_t i = 0; i < dna_len; i++)
 	{
-		dna_str[i] = base_array[base_chooser(gen)];
+		dna_str[i] = base_array[base_chooser(generator)];
 	}
 
 	dna_str[dna_len] = '\0';
