c9fd3bce7c5acdaebaceaa9f11963f433e021bb4
Diego Assencio <diego@assencio.com>
2017-06-09 08:29:09 +0200

    formats code

--- dna.cpp
+++ dna.cpp
@@ -7,52 +7,52 @@
 /**
  * @brief Generates a random DNA sequence with length in the range [1,20].
  */
-char* random_dna ()
+char* random_dna()
 {
-	static const char base_array[] = { 'A', 'C', 'G', 'T' };
+    static const char base_array[] = {'A', 'C', 'G', 'T'};
 
-	std::random_device device;
-	std::mt19937 generator(device());
+    std::random_device device;
+    std::mt19937 generator(device());
 
-	/* random DNA length generator */
-	std::uniform_int_distribution<int> length_chooser(1,20);
+    /* random DNA length generator */
+    std::uniform_int_distribution<int> length_chooser(1, 20);
 
-	/* random DNA base generator */
-	std::uniform_int_distribution<int> base_chooser(0,3);
+    /* random DNA base generator */
+    std::uniform_int_distribution<int> base_chooser(0, 3);
 
-	/* select the DNA sequence length */
-	size_t dna_len = length_chooser(generator);
+    /* select the DNA sequence length */
+    size_t dna_len = length_chooser(generator);
 
-	char* dna_str = new char[dna_len+1];
+    char* dna_str = new char[dna_len + 1];
 
-	/* build the DNA sequence */
-	for (size_t i = 0; i < dna_len; ++i)
-	{
-		dna_str[i] = base_array[base_chooser(generator)];
-	}
+    /* build the DNA sequence */
+    for (size_t i = 0; i < dna_len; ++i)
+    {
+        dna_str[i] = base_array[base_chooser(generator)];
+    }
 
-	dna_str[dna_len] = '\0';
+    dna_str[dna_len] = '\0';
 
-	return dna_str;
+    return dna_str;
 }
 
-int main ()
+int main()
 {
-	const char* dna_str_original = random_dna();
+    const char* dna_str_original = random_dna();
 
-	DnaBitset dna(dna_str_original, std::strlen(dna_str_original));
+    DnaBitset dna(dna_str_original, std::strlen(dna_str_original));
 
-	const char* dna_str_recovered = dna.to_string();
+    const char* dna_str_recovered = dna.to_string();
 
-	std::cout << "original sequence : " << dna_str_original  << "\n";
-	std::cout << "recovered sequence: " << dna_str_recovered << "\n";
+    std::cout << "original sequence : " << dna_str_original << "\n";
+    std::cout << "recovered sequence: " << dna_str_recovered << "\n";
 
-	int result = std::strcmp(dna_str_original, dna_str_recovered);
+    int result = std::strcmp(dna_str_original, dna_str_recovered);
 
-	std::cout << (result == 0 ? "test passed" : "test failed") << "\n";
+    std::cout << (result == 0 ? "test passed" : "test failed") << "\n";
 
-	delete[] dna_str_original;
-	delete[] dna_str_recovered;
+    delete[] dna_str_original;
+    delete[] dna_str_recovered;
 
-	return (result == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
+    return (result == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
 }
--- dna.h
+++ dna.h
@@ -1,115 +1,115 @@
 #ifndef __dna_h__
 #define __dna_h__
 
-#include <cstring>     /* std::memset */
-#include <stdexcept>   /* std::invalid_argument */
+#include <cstring>   /* std::memset */
+#include <stdexcept> /* std::invalid_argument */
 
-#define BASE_MASK 0x3   /* binary: 11 */
+#define BASE_MASK 0x3 /* binary: 11 */
 
 /* useful constants */
 enum
 {
-	BASE_A = 0x0,   /* binary: 00 */
-	BASE_C = 0x1,   /* binary: 01 */
-	BASE_G = 0x2,   /* binary: 10 */
-	BASE_T = 0x3,   /* binary: 11 */
+    BASE_A = 0x0, /* binary: 00 */
+    BASE_C = 0x1, /* binary: 01 */
+    BASE_G = 0x2, /* binary: 10 */
+    BASE_T = 0x3, /* binary: 11 */
 };
 
 class DnaBitset
 {
 public:
-	/**
-	 * @brief Constructs a compressed representation of a DNA sequence.
-	 * @param dna_str A string holding a DNA sequence (e.g. "ATGCACG").
-	 * @param dna_len The length of the DNA sequence.
-	 */
-	DnaBitset(const char* dna_str, const size_t dna_len)
-	{
-		m_len = dna_len;
+    /**
+     * @brief Constructs a compressed representation of a DNA sequence.
+     * @param dna_str A string holding a DNA sequence (e.g. "ATGCACG").
+     * @param dna_len The length of the DNA sequence.
+     */
+    DnaBitset(const char* dna_str, const size_t dna_len)
+    {
+        m_len = dna_len;
 
-		/* number of bytes necessary to store dna_str as a bitset */
-		size_t dna_bytes = (dna_len / 4) + (dna_len % 4 != 0);
+        /* number of bytes necessary to store dna_str as a bitset */
+        size_t dna_bytes = (dna_len / 4) + (dna_len % 4 != 0);
 
-		m_data = new uint8_t[dna_bytes];
+        m_data = new uint8_t[dna_bytes];
 
-		std::memset(m_data, 0, dna_bytes);
+        std::memset(m_data, 0, dna_bytes);
 
-		/* for each base of the DNA sequence */
-		for (size_t i = 0; i < dna_len; ++i)
-		{
-			uint8_t shift = 6 - 2*(i % 4);
+        /* for each base of the DNA sequence */
+        for (size_t i = 0; i < dna_len; ++i)
+        {
+            uint8_t shift = 6 - 2 * (i % 4);
 
-			switch (dna_str[i])
-			{
-			case 'A':
-				m_data[i/4] |= BASE_A << shift;
-				break;
-			case 'C':
-				m_data[i/4] |= BASE_C << shift;
-				break;
-			case 'G':
-				m_data[i/4] |= BASE_G << shift;
-				break;
-			case 'T':
-				m_data[i/4] |= BASE_T << shift;
-				break;
-			default:
-				throw std::invalid_argument("invalid DNA base");
-			}
+            switch (dna_str[i])
+            {
+                case 'A':
+                    m_data[i / 4] |= BASE_A << shift;
+                    break;
+                case 'C':
+                    m_data[i / 4] |= BASE_C << shift;
+                    break;
+                case 'G':
+                    m_data[i / 4] |= BASE_G << shift;
+                    break;
+                case 'T':
+                    m_data[i / 4] |= BASE_T << shift;
+                    break;
+                default:
+                    throw std::invalid_argument("invalid DNA base");
+            }
 
-			shift = (shift == 0) ? 6 : shift-2;
-		}
-	}
+            shift = (shift == 0) ? 6 : shift - 2;
+        }
+    }
 
-	/**
-	 * @brief Destructor.
-	 */
-	~DnaBitset()
-	{
-		delete[] m_data;
-	}
+    /**
+     * @brief Destructor.
+     */
+    ~DnaBitset()
+    {
+        delete[] m_data;
+    }
 
-	/**
-	 * @brief Returns the stored DNA sequence as an ASCII string.
-	 */
-	char* to_string() const
-	{
-		char* dna_str = new char[m_len+1];
+    /**
+     * @brief Returns the stored DNA sequence as an ASCII string.
+     */
+    char* to_string() const
+    {
+        char* dna_str = new char[m_len + 1];
 
-		/* for each base of the DNA sequence */
-		for (size_t i = 0; i < m_len; ++i)
-		{
-			uint8_t shift = 6 - 2*(i % 4);
+        /* for each base of the DNA sequence */
+        for (size_t i = 0; i < m_len; ++i)
+        {
+            uint8_t shift = 6 - 2 * (i % 4);
 
-			/* get the i-th DNA base */
-			uint8_t base = (m_data[i/4] & (BASE_MASK << shift)) >> shift;
+            /* get the i-th DNA base */
+            uint8_t base = (m_data[i / 4] & (BASE_MASK << shift)) >> shift;
 
-			switch (base)
-			{
-			case BASE_A:
-				dna_str[i] = 'A';
-				break;
-			case BASE_C:
-				dna_str[i] = 'C';
-				break;
-			case BASE_G:
-				dna_str[i] = 'G';
-				break;
-			case BASE_T:
-				dna_str[i] = 'T';
-				break;
-			default:
-				throw std::runtime_error("invalid DNA base");
-			}
-		}
+            switch (base)
+            {
+                case BASE_A:
+                    dna_str[i] = 'A';
+                    break;
+                case BASE_C:
+                    dna_str[i] = 'C';
+                    break;
+                case BASE_G:
+                    dna_str[i] = 'G';
+                    break;
+                case BASE_T:
+                    dna_str[i] = 'T';
+                    break;
+                default:
+                    throw std::runtime_error("invalid DNA base");
+            }
+        }
 
-		dna_str[m_len] = '\0';
-		return dna_str;
-	}
+        dna_str[m_len] = '\0';
+        return dna_str;
+    }
 
 private:
-	uint8_t* m_data;
-	size_t   m_len;
+    uint8_t* m_data;
+    size_t m_len;
 };
 
 #endif /* __dna_h__ */
